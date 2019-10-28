7cbe117ddc22ec5533b96bf57639663979a59735
byellin <41709605+byellin@users.noreply.github.com>
2019-10-28 15:54:23 -0400

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,13 +1,10 @@
 /*
 client.js
-
 Author: Nikolas Martelaro (nmartelaro@gmail.com)
 Extended: David Goeicke (da.goedicke@gmail.com)
 Purpose: This run the interactivity and communication for the web app. This file
 is served to the users web browser and executes on the browser.
-
 Usage: This file is called automatically when the webpage is served.
-
 //--Addition. Added a button handling for the `Take a picture` button.
 */
 
